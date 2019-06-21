c1a44e79e0f3f6b8a41b757445a516932a1535ab
InfamousMafia <50261821+InfamousMafia@users.noreply.github.com>
2019-06-21 10:12:52 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -206,14 +206,24 @@ ABUSE_STRINGS = [
 	   "`Ja be Gaandu`",
 	   "`Ma ka Bhodsa madharchod`",
 	   "`mml`",
+	   "`You MotherFukcer`",
 ]
 GEY_STRINGS = [
-     "`gey bsdk`",
+     "`you gey bsdk`",
      "`you gey`",
-     "`gey in the house`",
-     "`chakka here`",
-     "`gey gey gey gey gey gey gey gey`",
+     "`you gey in the house`",
+     "`you chakka`",
+     "`you gey gey gey gey gey gey gey gey`",
      "`you gey go away`",
+]
+PRO_STRINGS = [
+     "`This gey is pro as phack.`",
+     "`Pros here -_- Time to Leave`",
+     "`Muh Me Lega Bhosdike ?`",
+]
+ABUSEHARD_STRING = [
+	"`Madarchod Randi ke bacche.Oye bosdike madarchod bhen ke lode tere gand me lohe ka danda garam karke dalu randwe tujhetho gali ke kutte gand pe chut rakh ke katenge me bata raha hu tere lode pe madhu makkhi Katelode ke ando pe Road roller chale tu kab bathroom me muthne Jaye tho Tera loda ghir Jaye fir tere ando me se lizard ke bacche nikle teko kidnap Kare aur childporn banaye maa ke chuttad ke lode tere saat Johnny sins rape Kare aur jab wo teko anal de tab loda andar fas Jaye bkl tere jhaat pe waxing karunga me dhek lio fir jab tu chillayega na tab tere muh me Mai gai ka gobar dalunga sale tere gand ke balo pe tel laga ke jala du me teko Anaconda leke gand me dalu tho muh se nikle maa ke lode hamesha chutiyo jaisa bartav kartha he tu maa ke Dai chawal drugs tere gand Me dalunga thi tatti nahi nikle maa darchod kabhi teko Marne ka mouka mil gaya na tho bas I'll do my best to get that tatti outof you aur tere jaise chutio ko is duniya me jagaha bhi nahi maa ke lode bandarchod tere gand me chitiya Kate wo bhi bullet ants maadarchod samj nahi aaraha tere baap NE teko kya khake paida kiya Tha kesa chutiya he tu rand ke bacche teko shadi me khana khane na mile teko gand pe 4 thappad mare sab log aur blade se likhe I want anal madarchod bosdike maccharki tatte ke baal chutiye maa ke chut pe ghode ka Lund tere gand me jaltha hu koila Dale bhen ke lode MAA KI CHUT MAI TALWAR DUNGA BC CHUT FAT JAEGI AUR USME SE ITNA KHOON NIKLEGA MZA AJAEGA DEKHNE KA SALE MAA KE BHOSDE SE BAHR AJA FIR BAAP SE ZUBAN DA TERI MAA KI CHUT CHOD CHOD KE BHOSDABNADU MADARCHOD AUR USKE UPAR CENENT LAGADU KI TERE JESA GANDU INSAAN KABHI BAHR NA A SKE ESI GANDI CHUT MAI SE LODA LASUN MADRCHOD TERI MAA KI CHUT GASTI AMA KA CHUTIA BACHA TERI MAA KO CHOD CHOD K PAGAL KAR DUNGA MAA K LODY KISI SASTIII RANDII K BACHY TERI MAA KI CHOOT MAIN TEER MAARUN GANDU HARAMI TERI COLLEGE JATI BAJI KA ROAD PEY RAPE KARONGANDU KI OLAAD HARAM KI NASAL PAPA HUN TERA BHEN PESH KAR AB PAPA KO TERI MAA KKALE KUSS MAIN KIS`",
+	"`Main roz teri behno ki banjar chut me apna lawda daalke andar haryali lata tha magar aaj unke ke baare me sunke mujhe bhut afsos huwa..ki unko ab bada loudha chahye..ab mera balatkaaari lawda lagataar 4 ghante tk apne muh me kon rakhega..vo teri behne hi thi jo apni kaali magar rasilli chut mere saamne khol deti aur zameen pe naagin ki tarah rengne lgti thi jaise ki kisine unki chut pe naariyal tod diya ho vo b bada wala mumbai ka naariyal..apni chennal maa ko b nhi bhej rahe mere paas to main kaixe tum logo se vaada karu ki main teri maa chodd dungaw..ab agar tun sach me chahta hai ki main tum dono k mc ki chut me dhammal karu to mera lawda apne muh me rakho aur kaho Sameer hamare sage papa hain... Aur agar tb b the apni maa ki kaali chut mere saamne nahi rakhi to tumhare ghar me ghuske tumhari maa ka balatkaar kar dungaw jaixe delhi me huwa tha...ab teri chudi hui kuttiyo ki tarah apni gaand hilaate hue mere aage kalapna mt ni to tumhari fatti bhoxdi me 100 ched karunga`",
 ]
 # ===========================================
 
@@ -368,7 +378,16 @@ async def hacking (hacked):
                 "`Pay 6969$ to` @Thanos_ur_father `To Remove This Hack...`\n"
             )
 
-
+@register(outgoing=True, pattern="^.kill$")
+async def killing (killed):
+    """ Dont Kill Too much -_-"""
+    if not killed.text[0].isalpha() and killed.text[0] not in ("/", "#", "@", "!"):
+        if await killed.get_reply_message():
+            await killed.edit(
+                "`Targeted User was Killed successfully 😈......`\n"
+            )
+			  
+			  
 @register(outgoing=True, pattern="^.owo(?: |$)(.*)")
 async def faces(owo):
     """ UwU """
@@ -502,6 +521,23 @@ async def raping (raped):
         await raped.edit(reply_text)
                           
   
+@register(outgoing=True, pattern="^.pro$")
+async def proo (pros):
+    """ String for Pros only -_-"""
+    if not pros.text[0].isalpha() and pros.text[0] not in ("/", "#", "@", "!"):
+        index = random.randint(0, len(PRO_STRINGS) - 1)
+        reply_text = PRO_STRINGS[index]
+        await pros.edit(reply_text)
+			  
+			  
+@register(outgoing=True, pattern="^.abusehard$")
+async def fuckedd (abusehard):
+    """ Dont Use this Too much bsdk -_-"""
+    if not abusehard.text[0].isalpha() and abusehard.text[0] not in ("/", "#", "@", "!"):
+        index = random.randint(0, len(ABUSEHARD_STRING) - 1)
+        reply_text = ABUSEHARD_STRING[index]
+        await abusehard.edit(reply_text)
+			  
 			  
 @register(outgoing=True, pattern="^.gey$")
 async def geys (geyed):
